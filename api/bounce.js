b07294773238323213da61ad1a42fb73d9ba2f8d
handynorm <handynorm@gmail.com>
2026-02-08 14:57:17 -0800

    Change fetch to await for nextNode request

--- api/bounce.js
+++ api/bounce.js
@@ -87,14 +87,13 @@ export default async function handler(req, res) {
     // silent
   }
 
-   if (nextNode && spore.PELAGOS.hops_remaining > 0) {
-    fetch(nextNode, {
+  if (nextNode && spore.PELAGOS.hops_remaining > 0) {
+    await fetch(nextNode, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(spore),
     }).catch(() => {});
   }
-
 
   return res.status(200).json({
     status: "bounced",
